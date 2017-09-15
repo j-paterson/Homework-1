a053d2f3b2fdabd9a234530be97a3af9e8ba53af
Jesse Paterson <jesse.r.pat@gmail.com>
2017-09-15 00:56:35 -0700

    Minimum completed

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -1,15 +1,20 @@
 pragma solidity ^0.4.13;
 
-
 contract greeter {
 	/* Add one variable to hold our greeting */
-	string greeting;
+	string greeting = "";
+	uint count = 0;
+	event printGreeting(string _msg);
 
 	function greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting;
 	}
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		count+=1;
+		printGreeting(greeting);
+		return greeting;
 	}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,8 +2,20 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+	uint previous = 1;
+	uint current = 1;
+
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+		uint count = 0;
+		if(position<=1){
+			return previous;
+		}
+		if(position<=2){
+			return current;
+		}
+		while(count < position){
+			return calculate(position-1)+calculate(position-2);
+		}
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
